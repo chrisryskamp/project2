9cbe352b3f6ad9661612d063eaa87e9216ab9d9b
mango <elig7393@gmail.com>
2019-04-01 22:07:45 -0500

    updated styles

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,15 +3,20 @@ var db = require("../models");
 module.exports = function(app) {
   // Load home page
   app.get("/", function(req, res) {
-      res.render("index");
+      res.render("landing");
   });
   // Load trivia form page
   app.get("/api/trivia", function(req, res) {
       res.render("triviaForm");
   });
 
+  app.get("/api/form", function(req, res) {
+    res.render("index");
+  });
+  
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
+
 };
